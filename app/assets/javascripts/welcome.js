ca105dfe1a654b7c91c7f2955c5dfc11942f396f
Janet Yi <janetyi@janets-mbp.att.net>
2014-05-15 16:49:27 -0700

    Creates AJAX request to send English text to server-side

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,13 +7,31 @@ View = function(){};
 
 Controller.prototype = {
   bindEvents: function(){
-    $( '.text-box textarea' ).keyup( view.showGrammarCheckedText )
+    // $( '.text-box textarea' ).keyup( view.showGrammarCheckedText )
+    $( '.text-box textarea' ).keyup( function(e){
+      e.preventDefault();
+      controller.createTranslationRequest()
+    })
+  },
+
+  createTranslationRequest: function(){
+    $.ajax({
+      type: "POST",
+      url: "/translations",
+      data: { sentence: $( '.text-box textarea' ).val() }
+    }).done(function(data){
+      // view.renderTranslatedText(data)
+    })
   }
 }
 
 View.prototype = {
   showGrammarCheckedText: function(){
     $( '.grammar-checked' ).html($( '.text-box textarea' ).val() )
+  },
+
+  renderTranslatedText: function(data){
+    $( '.translated' ).html($( data ).val() )
   }
 }
 
