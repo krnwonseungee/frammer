4e46c89ad3b3c789e395dc0b8d49ff15f5004e7f
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-23 18:39:13 -0700

    Working on translating 1 word only.

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,6 +1,9 @@
 class TranslationsController < ApplicationController
-  def create
-    redirect_to root_path
-    puts "!!!! #{params["sentence"]}"
+  def generate
+    untranslated_sentence = params["sentence"]
+    translation_maker = Translation.new
+    @practice_word = translion_maker.get_translated_word(untranslated_sentence)
+    render :json => @practice_word
+    # redirect_to root_path
   end
 end
