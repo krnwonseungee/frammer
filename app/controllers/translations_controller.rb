7016e7931cb885c8b65dc2bb3bd5452b93359d84
Janet Yi <janetyi@janets-mbp.att.net>
2014-05-24 15:07:43 -0700

    Refactors translation#generate route

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,9 +1,7 @@
 class TranslationsController < ApplicationController
   def generate
-    untranslated_sentence = params["sentence"]
-    translation_maker = Translation.new
-    @practice_word = translion_maker.get_translated_word(untranslated_sentence)
+    @practice_word = Translation.new.get_translated_word(params["sentence"])
+    # print "PRACTICE!!! #{@practice_word}"
     render :json => @practice_word
-    # redirect_to root_path
   end
 end
