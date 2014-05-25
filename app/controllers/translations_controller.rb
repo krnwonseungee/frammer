adddf3b553d7a95bccfcf98c15df1e80eedf32f4
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-24 18:54:10 -0700

    Refactors translation#generate route. remove print statement

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -1,7 +1,6 @@
 class TranslationsController < ApplicationController
   def generate
     @practice_word = Translation.new.get_translated_word(params["sentence"])
-    # print "PRACTICE!!! #{@practice_word}"
     render :json => @practice_word
   end
 end
