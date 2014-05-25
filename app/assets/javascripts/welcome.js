77c6ea55b1971e748a658f829b68e3147ebd43c2
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-24 18:53:40 -0700

    Removes 2nd AJAX request since it's not necessary

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -16,10 +16,10 @@ Controller.prototype = {
 
   createTranslationRequest: function(){
     $.ajax({
-      type: "POST",
+      type: "GET",
       url: "/generate",
       data: { sentence: $( '.text-box textarea' ).val() },
-      dataType: "text",
+      dataType: "json",
       success: function(data){
         console.log('french word sent!')
       },
@@ -28,40 +28,40 @@ Controller.prototype = {
         // console.log('french word not sent.')
       }
     }).done(function(data){
-      // console.log(data)
-      controller.retrieveTranslatedText(data)
+      var translation = data.tuc[0].phrase.text
+      view.renderTranslatedText(translation)
     })
   },
 
-  retrieveTranslatedText: function(){
-    $.ajax({
-      type: "GET",
-      url: "/generate",
-      // data: data,
-      dataType: "json",
-      success: function(data){
-        console.log(data)
-        console.log('english word retrieved!')
-      },
-      error: function(){
-        console.log('english word not retrieved.')
-      }
-    }).done(function(data){
-      console.log(data)
-      console.log(Object.keys(data))
-      $( '.translated' ).html($( data ).val() )
-      // view.renderTranslatedText(data)
-    })
-  }
+  // retrieveTranslatedText: function(){
+  //   $.ajax({
+  //     type: "GET",
+  //     url: "/generate",
+  //     // data: data,
+  //     dataType: "json",
+  //     success: function(data){
+  //       console.log(data)
+  //       console.log('english word retrieved!')
+  //     },
+  //     error: function(){
+  //       console.log('english word not retrieved.')
+  //     }
+  //   }).done(function(data){
+  //     // console.log(Object.keys(data))
+  //     // if (data)
+  //     $( '.translated' ).html($( data ).val() )
+  //     // view.renderTranslatedText(data)
+  //   })
+  // }
 }
 
 View.prototype = {
-  showGrammarCheckedText: function(){
-    $( '.grammar-checked' ).html($( '.text-box textarea' ).val() )
-  },
+  // showGrammarCheckedText: function(){
+  //   $( '.grammar-checked' ).html($( '.text-box textarea' ).val() )
+  // },
 
   renderTranslatedText: function(data){
-    $( '.translated' ).html($( data ).val() )
+    $( '.translated' ).html( data )
   }
 }
 
