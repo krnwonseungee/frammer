2096d9ca8763d2943723e3949fc03311f540fc48
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-26 16:35:33 -0700

    Removes initialize method; incorporates db lookup into transl algorithm

--- app/models/translator.rb
+++ app/models/translator.rb
@@ -2,16 +2,17 @@ class Translator < ActiveRecord::Base
   include HTTParty
   base_uri 'http://glosbe.com/gapi/'
 
-  # def initialize(args)
-  #   @french_word = args[:french_word]
-  #   @eng_word = args[:english_word]
-  #   @options = {  query: {from: 'fra', dest: 'eng', format: 'json'} }
-  #   @eng_sent_arr = Array.new
-  # end
-
   def get_english_sentence(french_sentence)
+    @eng_sent_arr = Array.new
     french_sentence.split(" ").each do |fr_word|
-      @eng_sent_arr << (get_translated_word(fr_word) + " ")
+      if Translator.where("french_word like ?", fr_word ).exists?
+        translated_word = get_translated_word_via_db(fr_word) + " "
+        puts "THRU DB #{translated_word}"
+      else
+        translated_word = get_translated_word_via_api(fr_word) + " "
+        puts "THRU API #{translated_word}"
+      end
+      @eng_sent_arr << translated_word
     end
     @eng_sent_arr
   end
@@ -20,8 +21,12 @@ class Translator < ActiveRecord::Base
     sent_str.split(" ")
   end
 
-  def get_translated_word(untranslated_word)
-    @options[:query][:phrase] = untranslated_word
+  def get_translated_word_via_db(untranslated_word)
+    Translator.where("french_word like ?", fr_word ).first.english_word
+  end
+
+  def get_translated_word_via_api(untranslated_word)
+    @options = {  query: {from: 'fra', dest: 'eng', format: 'json', phrase: untranslated_word } }
     self.class.get("/translate", @options)["tuc"][0]["phrase"]["text"]
   end
 
