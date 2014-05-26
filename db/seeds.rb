83ced0a5d2aba7c421aabc25768a4c0e362b5504
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-26 16:35:11 -0700

    Added more possible delimiters in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 IO.foreach("public/dictionary.txt").each do |line|
-  delimiter = ["(f)", "(m)", "(m'f)", "  "].select{|el| line.include?(el) == true }.first
+  delimiter = ["(f)", "(m)", "(m'f)", "(m'p)", "(f'p)", "  "].select{|el| line.include?(el) == true }.first
   split_definitions_arry = line.split(delimiter)
   french_term = split_definitions_arry.shift
   Translator.create(french_word: french_term, english_word: split_definitions_arry.join(" "))
