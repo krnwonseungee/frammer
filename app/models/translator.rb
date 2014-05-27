1ef692adcff954ee805a5623f433ab4dca54d1db
Janet Yi <janetyi@Janets-MacBook-Pro.local>
2014-05-27 15:41:24 -0700

    Refactors translator methods to make more modular

--- app/models/translator.rb
+++ app/models/translator.rb
@@ -5,29 +5,38 @@ class Translator < ActiveRecord::Base
   def get_english_sentence(french_sentence)
     @eng_sent_arr = Array.new
     french_sentence.split(" ").each do |fr_word|
-      if Translator.where("french_word like ?", fr_word ).exists?
-        translated_word = get_translated_word_via_db(fr_word) + " "
-        puts "THRU DB #{translated_word}"
-      else
-        translated_word = get_translated_word_via_api(fr_word) + " "
-        puts "THRU API #{translated_word}"
-      end
-      @eng_sent_arr << translated_word
+      @eng_sent_arr << get_translated_word(fr_word)
     end
     @eng_sent_arr
   end
 
-  def split_words(sent_str)
-    sent_str.split(" ")
+  def get_translated_word(french_word)
+    if Translator.where("french_word like ?", french_word + "%" ).length != 0
+      puts "THRU DB #{french_word}"
+      find_translated_word_via_db(french_word)
+    else
+      puts "THRU API #{french_word}"
+      find_translated_word_via_api(french_word) + " "
+    end
   end
 
-  def get_translated_word_via_db(untranslated_word)
-    Translator.where("french_word like ?", fr_word ).first.english_word
+  private
+
+  def find_translated_word_via_db(untranslated_word)
+    Translator.where("french_word like ?", untranslated_word + "%" ).first.english_word + " "
   end
 
-  def get_translated_word_via_api(untranslated_word)
+  def find_translated_word_via_api(untranslated_word)
     @options = {  query: {from: 'fra', dest: 'eng', format: 'json', phrase: untranslated_word } }
     self.class.get("/translate", @options)["tuc"][0]["phrase"]["text"]
+  end
+
+  def recognize_proper_nouns(untranslated_word)
+
+  end
+
+  def translate_plural_noun(untranslated_word)
+
   end
 
 end
